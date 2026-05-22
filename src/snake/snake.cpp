83f273dc6cf748c3beabde61974430d2a35306ae
Aashutosh-kc <aashutoshkc853@gmail.com>
2026-05-22 09:51:28 +0545

    feat: add a moving head with WASD keys

--- src/snake/snake.cpp
+++ src/snake/snake.cpp
@@ -1,13 +1,27 @@
 #include <SFML/Graphics.hpp>
 void runSnake(sf::RenderWindow& window) {
 
-	sf::RectangleShape rect({ 100.f,100.f });
+	const int CELL_SIZE = 20;
+	auto size = window.getSize();
+	const int COLS = size.x / CELL_SIZE;
+	const int ROWS = size.y / CELL_SIZE;
+
+	sf::RectangleShape rect({
+		static_cast<float> (CELL_SIZE),
+		static_cast<float>(CELL_SIZE)
+		});
+
 	rect.setFillColor(sf::Color::White);
-	rect.setPosition({ 100.f,500.f });
+	sf::Vector2i position(5, 5);
+
+	sf::Clock clock;
+	float moveInterval = 0.2f;
+
+	sf::Vector2i direction(1, 0);
 
 	while (window.isOpen()) {
 		while (auto e = window.pollEvent()) {
-			
+
 			if (e->is<sf::Event::Closed>())
 				window.close();
 			if (e->is<sf::Event::KeyPressed>())
@@ -18,16 +32,39 @@ void runSnake(sf::RenderWindow& window) {
 			}
 		}
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
-			rect.move({0.f,-5.f});
+		/*if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+			rect.move({ 0.f,-5.f });
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
-			rect.move({-5.f,0.f});
+			rect.move({ -5.f,0.f });
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
-			rect.move({0.f,5.f});
+			rect.move({ 0.f,5.f });
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
-			rect.move({5.f,0.f});
+			rect.move({ 5.f,0.f });*/
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+			direction = { 0,-1 };
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+			direction = { -1,0 };
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+			direction = { 0,1 };
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+			direction = { 1,0 };
 
 
+		if (clock.getElapsedTime().asSeconds() >= moveInterval) {
+			position += direction;
+			clock.restart();
+
+			if (position.x < 0 || position.y < 0 || position.x >= COLS || position.y >= ROWS)
+				window.close();
+		}
+
+		rect.setPosition(
+			{
+			static_cast<float>(position.x * CELL_SIZE),
+			static_cast<float>(position.y * CELL_SIZE)
+			}
+		);
 		window.clear(sf::Color(10, 10, 10));
 		window.draw(rect);
 		window.display();
