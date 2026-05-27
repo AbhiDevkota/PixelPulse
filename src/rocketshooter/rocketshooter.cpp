5631614a75ea1eb314dc8f2a951770926eb81bf3
samyam-5500-oooo <sanyamkhadka.1000@gmail.com>
2026-05-27 22:26:37 +0545

    feat: add initial game loop

--- src/rocketshooter/rocketshooter.cpp
+++ src/rocketshooter/rocketshooter.cpp
@@ -1,3 +1,28 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include<cstdlib>
+using namespace std;
 void RocketShooter(sf::RenderWindow& window){
-cout << "initial phase" << endl;
+cout << "Inital phase: RocketShooter!" << endl;
+sf::RectangleShape rocket({40.f,70.f});
+rocket.setFillColor(sf::Color:Green);
+rocket.setPosition({220.f, 500.f}); // Put it somewhere visible
+while (window.isOpen()) {
+        while (auto e = window.pollEvent()) {
+            if (e->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+        window.clear(sf::Color::Black);
+        window.draw(rocket);
+        window.display();
+    }
+}
+});
+int main() {
+    //small window
+    sf::RenderWindow window(sf::VideoMode({ 480, 640 }), "RocketShooter");
+    window.setFramerateLimit(60); //setting the performance frame rate
+    RocketShooter(window);  //calling main rocketshooter function
+    return 0;
 }
