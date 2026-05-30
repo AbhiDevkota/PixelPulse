5a9cbdfd9db2d04f0546ac4d99086c77b5004d94
Prabin Dhungana <prabindhungana000@gmail.com>
2026-05-30 16:29:25 +0545

    feat : add popat sprite

--- src/flappybird/flappybird.cpp
+++ src/flappybird/flappybird.cpp
@@ -2,7 +2,17 @@
 
 void runFlappyBird(sf::RenderWindow& window) {
 
-    
+    float cellW = (float)window.getSize().x / 12.f;
+    float cellH = (float)window.getSize().y / 16.f;
+
+    // bird
+    sf::Texture birdTexture;
+    if (!birdTexture.loadFromFile("assets/Popat.png")) return;
+    sf::Sprite popat(birdTexture);
+    float birdScaleX = (float)window.getSize().x / birdTexture.getSize().x * 0.05f;
+    float birdScaleY = (float)window.getSize().y / birdTexture.getSize().y * 0.1f;
+    popat.setScale({ birdScaleX, birdScaleY });
+    popat.setPosition({ cellW * 2.f, cellH * 8.f });
 
     // background
     sf::Texture bgTexture;
@@ -28,6 +38,7 @@ void runFlappyBird(sf::RenderWindow& window) {
         
         window.clear();
         window.draw(background);
+		window.draw(popat);
         
 
         window.display();
