a8ffe9a0f66d95381a74e05af8612f925d0b9ce9
Prabin Dhungana <prabindhungana000@gmail.com>
2026-05-30 17:45:22 +0545

    feat : add top pipe and bottom pipe sprites

--- src/flappybird/flappybird.cpp
+++ src/flappybird/flappybird.cpp
@@ -5,7 +5,7 @@ void runFlappyBird(sf::RenderWindow& window) {
     float cellW = (float)window.getSize().x / 12.f;
     float cellH = (float)window.getSize().y / 16.f;
 
-    // bird
+    //bird
     sf::Texture birdTexture;
     if (!birdTexture.loadFromFile("assets/Popat.png")) return;
     sf::Sprite popat(birdTexture);
@@ -14,7 +14,7 @@ void runFlappyBird(sf::RenderWindow& window) {
     popat.setScale({ birdScaleX, birdScaleY });
     popat.setPosition({ cellW * 2.f, cellH * 8.f });
 
-    // background
+    //background
     sf::Texture bgTexture;
     if (!bgTexture.loadFromFile("assets/Background.png")) return;
     sf::Sprite background(bgTexture);
@@ -23,6 +23,25 @@ void runFlappyBird(sf::RenderWindow& window) {
     background.setScale({ scaleX, scaleY });
 
  
+    //pipes
+    sf::Texture upTexture;
+    if (!upTexture.loadFromFile("assets/Up.png")) return;
+    sf::Sprite pipeUp(upTexture);
+    float pipeScaleX = (float)window.getSize().x / upTexture.getSize().x * 0.15f;
+    float pipeScaleY = (float)window.getSize().y / upTexture.getSize().y * 0.4f;
+    pipeUp.setScale({ pipeScaleX, pipeScaleY });
+
+    sf::Texture downTexture;
+    if (!downTexture.loadFromFile("assets/Down.png")) return;
+    sf::Sprite pipeDown(downTexture);
+    pipeDown.setScale({ pipeScaleX, pipeScaleY });
+
+    float pipeX = cellW * 12.f;
+    float gapY = cellH * 8.f;
+    float gapSize = cellH * 4.f;
+
+    pipeDown.setPosition({ pipeX, gapY - gapSize / 2.f - pipeDown.getGlobalBounds().size.y });
+    pipeUp.setPosition({ pipeX, gapY + gapSize / 2.f });
 
     while (window.isOpen()) {
 
@@ -38,7 +57,10 @@ void runFlappyBird(sf::RenderWindow& window) {
         
         window.clear();
         window.draw(background);
+        window.draw(pipeDown);
+        window.draw(pipeUp);
 		window.draw(popat);
+		
         
 
         window.display();
