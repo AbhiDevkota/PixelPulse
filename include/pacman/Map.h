eba9f535e9630c055aa1c21620f9c8f367eb9976
Abhi Devkota <abhidevkota1234@gmail.com>
2026-06-18 23:53:29 +0545

    feat: add initial header files

---
+++ include/pacman/Map.h
@@ -0,0 +1,8 @@
+#ifndef MAP_H
+#define MAPH
+
+
+
+
+
+#endif
---
+++ include/pacman/Pacman.h
@@ -0,0 +1,20 @@
+#ifndef PACMAN_H
+#define PACMAN_H
+
+#include <SFML/Graphics.hpp>
+#include "pacman/Map.h"
+
+enum class GameState {
+	PLAYING,
+	MENU,
+	GAME_OVER
+};
+
+class Pacman {
+	public:
+
+	private:
+};
+
+
+#endif
